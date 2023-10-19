d3c5272d4ac92b72d4bce1ca9bbae1fb04da1e6e
DanhQuy <danhquy1101@gmail.com>
2023-10-19 22:07:49 +0700

    Create Project

---
+++ src/main/java/com/example/doantotnghiep/DoantotnghiepApplication.java
@@ -0,0 +1,12 @@
+package com.example.doantotnghiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoantotnghiepApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DoantotnghiepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doantotnghiep/DoantotnghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoantotnghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
